24b528efee66102761e27d9adbfa44fd6f6aaad6
saibat <sailesh.bathala@gmail.com>
2022-02-28 18:26:48 +0530

    added 3 new ML model codes

--- model.py
+++ model.py
@@ -2,3 +2,10 @@
 
 
 print('adding all ML related models')
+
+ def model1():
+     print('Adding code for ML algorithm 1')
+def model2():
+     print('Adding code for ML algorithm 2')
+def model3():
+     print('Adding code for ML algorithm 3')
